99e7f8a2f95f31fdd9af89b53abd91c53fec8fa1
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-27 21:58:13 +0000

    automatically backed up by learn

--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -28,6 +28,10 @@ class FilteredFruitList extends Component {
 
 const FilteredFruitList = function (props) {
   const list = !props.filter || props.filter === 'all' ? props.items : props.items.filter(i => i.fruit_type === props.filter);
-
+  return (
+        <ul className="fruit-list">
+          {list.map((item, index) => <li key={index}>{item.char}</li>)}
+        </ul>
+        )
 }
 export default FilteredFruitList;
