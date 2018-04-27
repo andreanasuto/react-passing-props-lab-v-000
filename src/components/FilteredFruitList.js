3f14cb88416870b3b125967a460f97caa17a9cd8
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-27 21:58:01 +0000

    automatically backed up by learn

--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -15,16 +15,16 @@ class FilteredFruitList extends Component {
       .then(items => this.setState({ items }));
   }
 
-  render() {
-    const list = !this.props.filter || this.props.filter === 'all' ? this.state.items : this.state.items.filter(i => i.fruit_type === this.props.filter);
-
-    return (
-      <ul className="fruit-list">
-        {list.map((item, index) => <li key={index}>{item.char}</li>)}
-      </ul>
-    );
-  }
-}
+//  render() {
+//    const list = !this.props.filter || this.props.filter === 'all' ? this.state.items : this.state.items.filter(i => i.fruit_type === this.props.filter);
+//
+//    return (
+//      <ul className="fruit-list">
+//        {list.map((item, index) => <li key={index}>{item.char}</li>)}
+//      </ul>
+//    );
+//  }
+// }
 
 const FilteredFruitList = function (props) {
   const list = !props.filter || props.filter === 'all' ? props.items : props.items.filter(i => i.fruit_type === props.filter);
